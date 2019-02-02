a04a7a3dbbdca990d8f652489c0f9b750d1f0373
Joe Wheelock <joemwheelock@gmail.com>
2019-02-02 10:08:40 -0800

    started work on first js object

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@ function addJob(){
     const form1 =document.getElementById('workForm');
     const cln = form1.cloneNode(true); document.getElementById('workForm').appendChild(cln);
 }
+
+const finalResume = []
+
+const signUpSubmit = document.querySelector("#signUpSubmit");
+
+signUpSubmit.addEventListener('click', signUp);
+
+function signUp() {
+    finalResume.firstName = firstName.value;
+    finalResume.lastName = lastName.value;
+    finalResume.phoneNumber = phoneNumber.value;
+    finalResume.email = email.value;
+    finalResume.address = address.value; 
+}
