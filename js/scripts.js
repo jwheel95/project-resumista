ae302c8925f8a87e7d08d2334864c272b231adf2
Joe Wheelock <joemwheelock@gmail.com>
2019-02-08 15:17:15 -0800

    created function to increment skill list #s as forms added to work section

--- js/scripts.js
+++ js/scripts.js
@@ -53,17 +53,25 @@ function educationHistory() {
     finalResume.educationField1 = document.querySelector("#educationField1").value;
 }
 
-//Function to Update Ids of all inputs when forms are cloned
+//Function to Update Ids and onChange of all inputs when forms are cloned
 function updateIds(clone, newNum) {
     let inputs = clone.querySelectorAll("*");
     console.log(inputs);
     inputs.forEach(function(input) {
         if (input.id) {
             input.setAttribute("id", input.id.slice(0, -1) + newNum);
-        }
+        } 
     });
 }
 
+function updateOnChange(clone, newNum) {
+    let inputs = clone.querySelectorAll("*");
+    inputs.forEach(function(input) {
+        if (input.hasAttribute("onchange")) {
+            input.setAttribute("onchange", `changeSkillList(${newNum})`);
+        }
+    });
+}
 
 // ----------------- ADD/DELETE DUPLICATE FORM FEATURE ------------------------
 // TODO: Refactor way too much copied code, not sure how....
@@ -103,6 +111,7 @@ function addWork() {
     newElemCloned.setAttribute("id", `workEntry${newNum}`);
     // update childrens' ids
     updateIds(newElemCloned, newNum);
+    updateOnChange(newElemCloned, newNum);
 
     // Header Change
     let header = newElemCloned.querySelector(".heading-ref");
@@ -306,10 +315,10 @@ jobsAndSkills['inputGroupSelect4'] = ['','Lawn care', 'Irrigation', 'Ability to 
 // factory-warehouse
 jobsAndSkills['inputGroupSelect5'] = ['','Goal oriented', 'Effective interpersonal Communication', 'Hands on', 'Physical Stamina','Shipping and recieving','Catolog inventory', 'Operates heavy machinery','Reliable','Time mangement','Multitasking'];
 
-function ChangeSkillList() {
-  var workField1List = document.getElementById("workField1");
-  var workSkillList = document.getElementById("workSkills1");
-  var jobTitles = workField1List.options[workField1List.selectedIndex].value;
+function changeSkillList(num) {
+  var workFieldList = document.getElementById("workField" + num);
+  var workSkillList = document.getElementById("workSkills" + num);
+  var jobTitles = workFieldList.options[workFieldList.selectedIndex].value;
   while (workSkillList.options.length) {
     workSkillList.remove(0);
   }
@@ -317,8 +326,8 @@ function ChangeSkillList() {
   if (jobs) {
     var i;
     for (i = 0; i < jobs.length; i++) {
-      var workField1 = new Option(jobs[i], i);
-      workSkillList.options.add(workField1);
+      var workField = new Option(jobs[i], i);
+      workSkillList.options.add(workField);
     }
   }
 }
